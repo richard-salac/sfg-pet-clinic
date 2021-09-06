16ded6b76c6ba48e5346db623fe0e15e87910cbd
Richard Salac <richard.salac@gmail.com>
2021-09-06 14:25:57 +0200

    Section 4 - 64. Spring Pet Clinic - Create Index Page and Controller

---
+++ pet-clinic-web/src/test/java/guru/springframework/sfgpetclinic/controllers/IndexController.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package guru.springframework.sfgpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ *
+ * @author Richard Salac (richard.salac@datera.cz)
+ */
+@Controller
+public class IndexController {
+    
+    @RequestMapping({"/","","index","index.html"})
+    public String index(){
+        return "index";
+    }
+}
